51c8395bb48f0cf361303ad02f3dcc72d28261e1
d-Construct <campbell.michael@student.oaklandcc.edu>
2023-05-23 15:37:03 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+hours_worked_this_week = int(input('How many hours did you work this week? Numbers only*'))
+name_of_employee = input('What is your Full Name')
+hourly_rate = int(input('What is your current hourly wage?'))
+gross_pay = hours_worked_this_week * hourly_rate
+print(name_of_employee)
+print(gross_pay)
+print("is your gross pay")
+net_pay = gross_pay * 0.9
+print(net_pay)
+print("is your net pay")
+print(hours_worked_this_week)
+print("is the number of hours worked this week.")
+print(hourly_rate)
+print('is your hourly rate')
+
+
